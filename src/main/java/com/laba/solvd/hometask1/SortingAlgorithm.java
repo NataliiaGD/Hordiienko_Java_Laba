999ce3a90c7026d479eed61b18c455d2278c0487
NataliiaGD <nataliagordiyenko8@gmail.com>
2023-11-01 21:52:10 +0200

    added second part of homework1

--- src/main/java/com/laba/solvd/hometask1/SortingAlgorithm.java
+++ src/main/java/com/laba/solvd/hometask1/SortingAlgorithm.java
@@ -1,13 +1,36 @@
 package com.laba.solvd.hometask1;
 
+import java.util.Arrays;
 
 public class SortingAlgorithm {
     public static void main(String[] args) {
         int [] array = {2,5,8,4,6,7};
-        quickSort(array);
+        quickSort(array, 0, array.length - 1);
+        System.out.println("Відсортований масив: " + Arrays.toString(array));
     }
-
-    public static void quickSort (int [] array){
-
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(array, low, high);
+            quickSort(array, low, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, high);
+        }
+    }
+    public static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j] < pivot) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+        swap(array, i + 1, high);
+        return i + 1;
+    }
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 }
+
