993a993bd763d47c5fcd24a9ba40ba7d1d9aa5b5
NataliiaGD <nataliagordiyenko8@gmail.com>
2023-11-02 08:47:01 +0200

    refactored PrintInfoAboutTheUser

--- src/main/java/com/laba/solvd/hometask1/PrintInfoAboutTheUser.java
+++ src/main/java/com/laba/solvd/hometask1/PrintInfoAboutTheUser.java
@@ -4,14 +4,8 @@ import java.util.Scanner;
 
 public class PrintInfoAboutTheUser {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter Name");
-        String Name = scanner.nextLine();
-        System.out.println("Enter Surname");
-        String Surname = scanner.nextLine();
-        System.out.println("Hello! Nice to see you" + ' ' + Name + ' ' + Surname + "!");
-
-
+        String name = args [0];
+        String surname = args [1];
+        System.out.println("Hello! Nice to see you" + ' ' + name + ' ' + surname + "!");
     }
 }
