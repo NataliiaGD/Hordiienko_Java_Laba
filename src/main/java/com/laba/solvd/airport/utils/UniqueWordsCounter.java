9a240fbeb4c75d375c310d5f3888caef12879487
NataliiaGD <nataliagordiyenko8@gmail.com>
2023-11-27 22:43:56 +0200

    optimised method logic

--- src/main/java/com/laba/solvd/airport/utils/UniqueWordsCounter.java
+++ src/main/java/com/laba/solvd/airport/utils/UniqueWordsCounter.java
@@ -7,27 +7,27 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class UniqueWordsCounter {
     private static final Logger LOGGER = LogManager.getLogger(UniqueWordsCounter.class);
 
     public static void main(String[] args) {
-        countUniqueWordsInFile("src/main/resources/file.txt");
+        countUniqueWordsInFile("src/main/resources/file.txt", "src/main/resources/output.txt");
 
     }
 
-    private static void countUniqueWordsInFile(String pathToFile) {
+    private static void countUniqueWordsInFile(String pathToFile, String pathToOutputFile) {
         try {
             File file = new File(pathToFile);
             String textFromFile = FileUtils.readFileToString(file);
             String[] wordsArray = StringUtils.split(textFromFile);
-            int uniqueWordsCount = 0;
+            Set<String> uniqueWords = new HashSet<>();
             for (String word : wordsArray) {
-                if (StringUtils.countMatches(textFromFile, word) == 1) {
-                    uniqueWordsCount++;
-                }
+                uniqueWords.add(word);
             }
-            FileUtils.writeStringToFile(new File("src/main/resources/output.txt"), "Number of the unique words is " + uniqueWordsCount);
+            FileUtils.writeStringToFile(new File(pathToOutputFile), "Number of the unique words is " + uniqueWords.size());
         } catch (IOException e) {
             LOGGER.error(e.getMessage());
         }
